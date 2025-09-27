76d0e2fb0a30cf9cdaa7d304ec9a0cf024653cc3
VuPhuc08 <vvphuc29082008@gmail.com>
2025-09-27 23:14:21 +0800

    Add files via upload

---
+++ godminton/player 1.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+var speed = 150
+var gravity = 700
+
+var vel = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("player left"):
+		vel.x -= speed
+	elif Input.is_action_just_pressed("player right"):
+		vel.x += speed
+		
