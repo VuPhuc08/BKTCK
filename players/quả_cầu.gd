29d33bdccb9e9e32347ad2962d1626384a8795de
VuPhuc08 <vuphuc29082008@gmai.com>
2025-10-31 20:21:11 +0800

    update

--- players/quả_cầu.gd
+++ players/quả_cầu.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
-@export var speed = 50
+var speed = 200
 var zone = false
+var zone2 = false
 
 func _physics_process(delta: float) -> void:
-	if zone:
-		velocity.x = speed  
-		velocity.y = speed  
-	else:
-		velocity.x = 0
-	
+	velocity.x = 0
+	velocity.y = 0
+	if zone && !zone2:
+		velocity.x = speed
+		velocity.y = -speed
+	elif zone2 && !zone:
+		velocity.x = -speed
+		velocity.y = -speed
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player1" or body.name == "player2":
+	if body.name == "player1":
 		zone = true
+	if body.name == "player2":
+		zone2 = true
+
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "player1" or body.name == "player2":
+	if body.name == "player1":
 		zone = false
+	if body.name == "player2":
+		zone2 = false
