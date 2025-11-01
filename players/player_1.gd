0b04d8774454a722e73fdc30d78db56a82fcc99c
VuPhuc08 <vvphuc29082008@gmail.com>
2025-11-01 20:43:16 +0800

    suacode

--- players/player_1.gd
+++ players/player_1.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var body
+var power_speed = 100
+var moment_power = 0
+var is_charg = false 
 
 func _physics_process(delta: float) -> void:
 	var movey := Input.get_axis("player down", "player up")
--- players/quả_cầu.gd
+++ players/quả_cầu.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-var speed = 350
+var speed = 470
 var zone = false
 var zone2 = false
+
 func _physics_process(delta: float) -> void:
 	if zone && !zone2:
 		velocity.x = speed
@@ -17,13 +18,13 @@ func _physics_process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player1":
 		zone = true
-	if body.name == "player2":
+	elif body.name == "player2":
 		zone2 = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "player1":
 		zone = false
-	if body.name == "player2":
+	elif body.name == "player2":
 		zone2 = false
 func _ready() -> void:
 		collision_layer = 1
