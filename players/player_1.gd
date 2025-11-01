679f2dc45d46c107b2924bcf235dc118811cf105
VuPhuc08 <vvphuc29082008@gmail.com>
2025-11-01 16:30:25 +0800

    update

---
+++ players/player_1.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	var movey := Input.get_axis("player down", "player up")
+	if movey:
+		velocity.y = movey * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+	var movex := Input.get_axis("player left", "player right")
+	if movex:
+		velocity.x = movex * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
