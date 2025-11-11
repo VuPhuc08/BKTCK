35a9721adf34b38cbf11605fae156c9102a51596
VuPhuc08 <vvphuc29082008@gmail.com>
2025-11-11 17:01:03 +0800

    update_code_players

--- players/player_1.gd
+++ players/player_1.gd
@@ -1,35 +1,40 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var delta_arrow = 0.0
 
 @onready var arrow = $arrow
 @onready var power1 = $"../CanvasLayer/TextureProgressBar"
 
+func _ready() -> void:
+	power1.max_value = 100
+	power1.min_value = 0
+
 func _process(delta: float) -> void:
 	var power_plus := Input.is_action_just_pressed("power_+")
 	var power_minus := Input.is_action_just_pressed("power_-")
+
 	if power_plus:
 		power1.value += 20
 	if power_minus:
 		power1.value -= 20
-
-func _ready() -> void:
-	power1.max_value = 100
-	power1.min_value = 0
 
 func _physics_process(delta: float) -> void:
 	var movey := Input.get_axis("player down", "player up")
 	if movey:
 		velocity.y = movey * SPEED
 	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 	var movex := Input.get_axis("player left", "player right")
 	if movex:
 		velocity.x = movex * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
-	var movearrowD := Input.get_action_strength("down1")
-	var movearrowU := Input.get_action_strength("up1")
-	arrow.rotation_degrees += (movearrowD - movearrowU) * 100 * delta
-	arrow.rotation_degrees = clamp(arrow.rotation_degrees, 0, 180)
+	if Input.is_action_just_pressed("down1"):
+		delta_arrow += 30
+	if Input.is_action_just_pressed("up1"):
+		delta_arrow -= 30
+	delta_arrow = clamp(delta_arrow, 0.0, 180.0)
+	arrow.rotation_degrees = delta_arrow
--- players/player_2.gd
+++ players/player_2.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var delta_arrow = 180.0
 
 @onready var arrow = $Node2D
 @onready var power = $"../CanvasLayer/TextureProgressBar2"
@@ -9,7 +10,7 @@ func _ready() -> void:
 	power.max_value = 100
 	power.min_value = 0
 
-func _process(delta: float) -> void:
+func _process(_float):
 	var power_plus := Input.is_action_just_pressed("power_++")
 	var power_minus := Input.is_action_just_pressed("power_--")
 	if power_plus:
@@ -17,20 +18,22 @@ func _process(delta: float) -> void:
 	if power_minus:
 		power.value -= 20 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_float):
 	var movey := Input.get_axis("ui_up", "down")
 	if movey:
 		velocity.y = movey * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 	var movex := Input.get_axis("ui_left", "ui_right")
 	if movex:
 		velocity.x = movex * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
-	var movearrowD := Input.get_action_strength("down2")
-	var movearrowU := Input.get_action_strength("up2")
-	arrow.rotation_degrees += (movearrowU - movearrowD) * 100 * delta
-	arrow.rotation_degrees = clamp(arrow.rotation_degrees, 180, 360)
+	if Input.is_action_just_pressed("up2"):
+		delta_arrow += 30
+	if Input.is_action_just_pressed("down2"):
+		delta_arrow -= 30
+	delta_arrow = clamp(delta_arrow, 180.0, 360.0)
+	arrow.rotation_degrees = delta_arrow
