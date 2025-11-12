8d8d28eaa2a589db1723dba9decde641751a947f
VuPhuc08 <vvphuc29082008@gmail.com>
2025-11-12 20:27:29 +0800

    update_code_cau

--- players/quả_cầu.gd
+++ players/quả_cầu.gd
@@ -7,7 +7,6 @@ const zone_note = 2
 
 var direction = Vector2.ZERO
 var stop = 0.0
-var delay = 0.0
 var zone1 = false
 var zone2 = false
 
@@ -44,7 +43,6 @@ func hit_by_player(player: Node2D, power: TextureProgressBar):
 	direction = Vector2(cos(goc), sin(goc)).normalized()
 	if player.name == "player2": 
 		direction = -Vector2(cos(goc), sin(goc)).normalized() 
-		direction.x = -abs(direction.x)
 	var power_delta = clamp(power.value/100, 0.2 , 1.0)
 	velocity = direction * speed * power_delta
 	$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -52,17 +50,24 @@ func hit_by_player(player: Node2D, power: TextureProgressBar):
 func teleport(delta: float):
 	var screen = get_viewport().get_visible_rect()
 	var note = note_position()
-
 	if global_position.x > screen.end.x + bien:
 		spawn_right()
 	elif global_position.x < screen.position.x - bien:
 		spawn_left()
+	elif global_position.y > screen.end.y + bien && velocity >= Vector2.ZERO:
+		spawn_right()
+	elif global_position.y < screen.position.y - bien && velocity >= Vector2.ZERO:
+		spawn_right()
+	elif  global_position.y > screen.end.y + bien && velocity  <= Vector2.ZERO:
+		spawn_left()
+	elif global_position.y < screen.position.y - bien && velocity <= Vector2.ZERO:
+		spawn_left()
 	elif abs(velocity.x) == 0.0 && !zone1 && !zone2 && !note:
 		stop += delta
 		if stop > 0.0:
-			if global_position.x > 67 || global_position.x < 576 || global_position.y > 90 || global_position.y < 557.75:
+			if global_position.x > 67 && global_position.x < 576 && global_position.y > 90 && global_position.y < 557.75:
 				spawn_right()
-			else:
+			elif global_position.x < 1086 && global_position.x > 576 && global_position.y > 90 && global_position.y < 557.75:
 				spawn_left()
 
 func note_position() -> bool:
