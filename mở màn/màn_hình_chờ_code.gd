36019cc8ea7ff2f017661f8ff066bc7a4c22e7c1
VuPhuc08 <vvphuc29082008@gmail.com>
2025-11-19 21:56:46 +0800

    update_code_cau

--- mở màn/màn_hình_chờ_code.gd
+++ mở màn/màn_hình_chờ_code.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Godminton/màn chơi/màn__chơi.tscn")
--- players/quả_cầu.gd
+++ players/quả_cầu.gd
@@ -56,16 +56,16 @@ func teleport(delta: float):
 	elif global_position.x < screen.position.x - bien:
 		spawn_left()
 		Global.goal_1 += 1
-	elif global_position.y > screen.end.y + bien && velocity >= Vector2.ZERO:
+	elif global_position.y > 600 && velocity >= Vector2.ZERO:
 		spawn_right()
 		Global.goal_2 += 1
-	elif global_position.y < screen.position.y - bien && velocity >= Vector2.ZERO:
+	elif global_position.y < 45 && velocity >= Vector2.ZERO:
 		spawn_right()
 		Global.goal_2 += 1
-	elif  global_position.y > screen.end.y + bien && velocity  <= Vector2.ZERO:
+	elif  global_position.y > 600 && velocity  <= Vector2.ZERO:
 		spawn_left()
 		Global.goal_1 += 1
-	elif global_position.y < screen.position.y - bien && velocity <= Vector2.ZERO:
+	elif global_position.y < 45 && velocity <= Vector2.ZERO:
 		spawn_left()
 		Global.goal_1 += 1
 	elif abs(velocity.x) == 0.0 && !zone1 && !zone2 && !note:
@@ -74,7 +74,13 @@ func teleport(delta: float):
 			if global_position.x > 67 && global_position.x < 576 && global_position.y > 90 && global_position.y < 557.75:
 				spawn_right()
 				Global.goal_2 += 1
-			elif global_position.x < 1086 && global_position.x > 576 && global_position.y > 90 && global_position.y < 557.75:
+			if global_position.x < 1086 && global_position.x > 576 && global_position.y > 90 && global_position.y < 557.75:
+				spawn_left()
+				Global.goal_1 += 1
+			if global_position.x > 1086:
+				spawn_right()
+				Global.goal_2 += 1
+			if global_position.x < 67:
 				spawn_left()
 				Global.goal_1 += 1
 
